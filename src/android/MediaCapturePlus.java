451ff5793f57c04b637d34399b66534790d6c9d9
Ethan <ethan@cortexinteractive.com.au>
2020-08-03 12:47:29 +0930

    Fixes

--- src/android/MediaCapturePlus.java
+++ src/android/MediaCapturePlus.java
@@ -34,7 +34,6 @@ public class MediaCapturePlus extends CordovaPlugin {
 
     private void openImageAssessment(CallbackContext callbackContext) {
         this.temp = callbackContext;
-        this.temp.success("Test js");
         Context context = this.cordova.getActivity().getApplicationContext();
         Intent intent = new Intent(context, ImageAssessmentActivity.class);
         this.cordova.startActivityForResult((CordovaPlugin) this, intent, 2);

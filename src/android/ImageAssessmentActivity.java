47b5a8a8325681ed9d495860c89905eacfa166b7
Ethan <ethan@cortexinteractive.com.au>
2020-08-04 09:38:34 +0930

    Fixes

--- src/android/ImageAssessmentActivity.java
+++ src/android/ImageAssessmentActivity.java
@@ -50,8 +50,8 @@ public class ImageAssessmentActivity extends Activity implements View.OnClickLis
         Resources resources = getApplication().getResources();
         setContentView(resources.getIdentifier("image_assessment_layout", "layout", package_name));
 
-        image = (ImageView) findViewById(resources.getIdentifier("textview", "id", package_name));
-        preview = (SurfaceView) findViewById(resources.getIdentifier("surface", "id", package_name));
+        image = findViewById(resources.getIdentifier("textview", "id", package_name));
+        preview = findViewById(resources.getIdentifier("surface", "id", package_name));
 
         previewHolder = preview.getHolder();
         previewHolder.addCallback(surfaceCallback);
